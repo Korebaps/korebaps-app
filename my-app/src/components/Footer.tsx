63ac9b536e11a62e5c40d82af2de9a4f7e829504
tkim2000 <106344190+tkim2000@users.noreply.github.com>
2026-01-30 17:53:04 -0500

    test

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
-import VisitorCounter from './VisitorCounter';
-
 export default function Footer() {
   return (
     <footer className="mt-10 border-t border-[#daaa00]/40 pt-6 text-center text-xs text-gray-500">
-      <p>
-        © 2026 Korebaps Stats Dashboard. All rights reserved.<br />
-        Made by Junsu Yoon(17), Taerim Kim(66)
-        <VisitorCounter />
-      </p>
+      <p>© 2026 Korebaps Stats Dashboard. All rights reserved.</p>
     </footer>
   );
 }
