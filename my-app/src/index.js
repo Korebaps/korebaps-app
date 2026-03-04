d48b68f890176b6388416b70e63f6bbbb906d743
tkim2000 <106344190+tkim2000@users.noreply.github.com>
2026-03-03 23:31:24 -0500

    Revert "."
    This reverts commit 52d13d0084cadfe065f482d8ecf5f58b25c043f1.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.tsx';
 import PlayerDetailPage from './pages/PlayerDetailPage.tsx';
 import AdminDashboard from './admin.tsx';
 import ActiveRosterPage from './pages/ActiveRosterPage.tsx';
